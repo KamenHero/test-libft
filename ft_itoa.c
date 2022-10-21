f921dd427ec4b5a492e067d40804f68f0e580825
Oussama Ryadi <oryadi@e2r9p8.1337.ma>
2022-10-21 18:51:02 +0100

    libft test

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,55 +12,83 @@
 
 #include "libft.h"
 
-static	int	ft_len(long int nb)
+static    int    ft_len(long int nb)
 {
-	int	i;
+    int    i;
 
-	i = 0;
-	while (nb)
-	{
-		nb /= 10;
-		i++;
-	}
-	return (i);
+    i = 0;
+    if(nb == 0)
+        i++;
+    while (nb)
+    {
+        nb /= 10;
+        i++;
+    }
+    return (i);
 }
 
-int	ft_check(long int nb, char c)
+static    int    ft_check(int nb)
 {
-	if (nb < 0)
-	{
-		nb *= -1;
-		c = '-';
-		return (1);
-	}
-	return (0);
+    if (nb < 0)
+    {
+        nb *= -1;
+
+        return (1);
+    }
+    return (0);
 }
 
-char	*ft_itoa(int n)
+char    *int_min(int n)
 {
-	char		*str;
-	char		c;
-	long int	nb;
-	int			len;
-	int			i;
+    int len;
+    unsigned int nb;
+    char    *str;
 
-	len = 0;
-	i = 0;
-	c = 'c';
-	nb = n;
-	if (nb < 0)
-		len++;
-	len += ft_len(nb);
-	str = malloc(sizeof (char) * (len +1));
-	str[len] = '\0';
-	while (nb != 0)
-	{
-		if (ft_check(n, c) == 1)
-		{
-			str[0] = c;
-		}
-		str[--len] = (nb % 10) + '0';
-		nb /= 10;
-	}
-	return (str);
+    len = ft_len(n) + 1;
+    nb = -n;
+    str = malloc(sizeof(char) * (len + 1));
+    str[0] = '-';
+    str[len] = '\0';
+    while (nb >= 1)
+    {
+        str[--len] = nb % 10 + '0';
+        nb /= 10; 
+    }
+    return (str);
+}
+
+char    *ft_itoa(int n)
+{
+    char        *str;
+    char        c;
+    int            len;
+    unsigned int  nb;
+
+    len = 0;
+    c = '-';
+    nb = n;
+    if (n < 0)
+    {
+        len++;
+        nb *= -1;
+    }
+    len += ft_len(n);
+    str = malloc(sizeof (char) * (len +1));
+    if (!str)
+        return (0);
+    if (n == -2147483648)
+        return (int_min(n));
+    str[len] = '\0';
+    if (n == 0)
+    {
+        str[0] = '0';
+    }
+    if (ft_check(n) == 1)
+        str[0] = c;
+    while (nb > 0)
+    {
+        str[--len] = nb % 10 + '0';
+        nb /= 10;
+    }
+    return (str);
 }
