1ea46364451bbb04929223db77012fb43ec32051
Oussama Ryadi <oryadi@e2r8p6.1337.ma>
2022-10-22 15:55:23 +0100

    libft test

--- ft_split.c
+++ ft_split.c
@@ -14,57 +14,81 @@
 
 int	sstart(const char *str, char c, int i)
 {
-	while (str[i] == c)
+	while (str[i] && str[i] == c)
 		i++;
 	return (i);
 }
 
 int	ft_len(const char *str, char c, int len)
 {
-	while (str[len] != c)
-		len++;
-	return (len);
-}
-
-int	ft_fstr(const char *str, char c)
-{
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-	while (str[i] == c)
+	while (str[len] && str[len++] != c)
+		i++;
+	return (i);
+}
+
+int    ft_fstr(const char *str, char c)
+{
+    int    i;
+    int    j;
+
+    i = 0;
+    j = 0;
+	while (str[i] != '\0')
 	{
-		while (str[i] != c)
-		{
-			if ((str[i] != c && str[i -1] == c) || (str[0] != c))
-				j++;
-			i++;
-		}
+		if ((str[0] != c) || (str[i] != c && str[i -1] == c))
+                j++;
 		i++;
 	}
 	return (j);
 }
 
+// int ft_fstr2(const char *str, char c)
+// {
+//     int    i;
+//     int    j;
+
+//     while (str[i] != '\0')
+//     {
+//         i = ft_fstr(str, c);
+//         while(str[i] == c)
+//         {
+//             if ((str[i] != c && str[i -1] == c) || (str[0] != c))
+//                 j++;
+//             if (str[i] != '\0')
+//                 return(j);
+//             return (i);
+//             i++;
+//         }
+//         i++;
+//     }
+//     return (0);
+// }
+
 char	**ft_split(char const *s, char c)
 {
 	int		i;
 	int		start;
-	int		len;
+	int		j;
 	char	**str;
+	int		len;
+
 
 	i = 0;
 	start = 0;
 	len = 0;
-	len += start;
-	**str = malloc(sizeof (char) * (ft_fstr(s, c) + 1));
-	while (s[len] != '\0')
+	j = 0; 
+	str = ft_calloc((ft_fstr(s, c) + 1),sizeof (char*) );
+	if (!str)
+		return (0);
+	while (j < (int)ft_strlen(s))
 	{
-		sstart(s, c, start);
-		len += start;
-		ft_len(s, c, len);
-		*str[i] = *ft_substr(s, start, len + start);
-		start += len;
+		start = sstart(s, c, j);
+		len = ft_len(s , c, start + j);
+		str[i] = ft_substr(s, j + start, len);
+		j += start + len;
 		i++;
 	}
 	return (str);
